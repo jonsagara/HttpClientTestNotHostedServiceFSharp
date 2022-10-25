e62d672e88d9c508f96a7e151d7d7bbed585efd1
Jon Sagara <jon@sagarasoftware.com>
2022-10-25 08:09:25 -0700

    Write to a log file.

--- HttpClientTestNotHostedServiceFSharp/TestService.fs
+++ HttpClientTestNotHostedServiceFSharp/TestService.fs
@@ -1,14 +1,20 @@
 ﻿namespace HttpClientTestNotHostedServiceFSharp
 
 open System.Net.Http
+open Serilog
 
 type TestService (httpClientFactory : IHttpClientFactory) =
+
+    static let _logger = Log.Logger.ForContext<TestService>()
 
     let _httpClient = httpClientFactory.CreateClient()
 
     member this.GetMicrosoftAsync() =
         task {
+            _logger.Information("Making HTTP request...")
             use requestMsg = new HttpRequestMessage(HttpMethod.Get, "https://www.microsoft.com")
             use! responseMsg = _httpClient.SendAsync(requestMsg)
+
+            _logger.Information("Done.")
             return! responseMsg.Content.ReadAsStringAsync()
         }
